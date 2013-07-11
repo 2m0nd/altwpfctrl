72451f8573a157b513232a526508e2b2e6f434d3
2m0nd <dime.vlasov@gmail.com>
2013-07-11 18:37:12 +0600

    Сделал что бы в дизайн моде выставлялся стиль у контрола кнопки, подглядел как сделано было в BusyWin8 Indicator.

--- Alt.Controls/Buttons/RoundedGeometryButton.cs
+++ Alt.Controls/Buttons/RoundedGeometryButton.cs
@@ -8,6 +8,14 @@ namespace Alt.Controls.Buttons
 {
 	public class RoundedGeometryButton : Button
 	{
+
+
+		static RoundedGeometryButton()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(RoundedGeometryButton), new FrameworkPropertyMetadata(typeof(RoundedGeometryButton)));
+		}
+
+
 		public static readonly DependencyProperty GeometryInRoundProperty =
 			DependencyProperty.Register("GeometryInRound", typeof(Geometry), typeof(RoundedGeometryButton), new PropertyMetadata(default(Geometry)));
 
